f9bfbfd62d76cb1aa54917f867beccec880b9f01
eddie <eddie@5xruby.tw>
2021-08-27 14:24:50 +0800

    show cart detail

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,4 +5,12 @@ class CartsController < ApplicationController
 
     redirect_to root_path, notice: "已加至購物車"
   end
+
+  def show
+  end
+
+  def destroy
+    session[:cart9527] = nil
+    redirect_to root_path, notice: "購物車已清除"
+  end
 end
